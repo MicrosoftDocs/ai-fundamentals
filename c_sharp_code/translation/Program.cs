e1a0407b110b438ce7bca3af5840411f4f82a663
Gerry O'Brien <gerry.obrien@microsoft.com>
2020-04-02 13:02:01 -0700

    Added c# code for translate text

---
+++ c_sharp_code/translation/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace translation
+{
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string subscriptionKey = "YOUR_COG_KEY";
+            string endpoint = "https://api.cognitive.microsofttranslator.com/";
+
+            // This is our main function.
+            // Output languages are defined in the route.
+            // For a complete list of options, see API reference.
+            // https://docs.microsoft.com/azure/cognitive-services/translator/reference/v3-0-translate
+            string route = "/translate?api-version=3.0&to=de&to=it&to=fr";
+
+            // Prompts you for text to translate. If you'd prefer, you can
+            // provide a string as textToTranslate.
+            Console.Write("Type the phrase you'd like to translate? ");
+            string textToTranslate = Console.ReadLine();
+            await TranslateTextRequest(subscriptionKey, endpoint, route, textToTranslate);
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+
+        }
+
+        // This sample requires C# 7.1 or later for async/await.
+        // Async call to the Translator Text API
+        static public async Task TranslateTextRequest(string subscriptionKey, string endpoint, string route, string inputText)
+        {
+            object[] body = new object[] { new { Text = inputText } };
+            var requestBody = JsonConvert.SerializeObject(body);
+
+            using (var client = new HttpClient())
+            using (var request = new HttpRequestMessage())
+            {
+                // Build the request.
+                // Set the method to Post.
+                request.Method = HttpMethod.Post;
+                // Construct the URI and add headers.
+                request.RequestUri = new Uri(endpoint + route);
+                request.Content = new StringContent(requestBody, Encoding.UTF8, "application/json");
+                request.Headers.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
+
+                // Send the request and get response.
+                HttpResponseMessage response = await client.SendAsync(request).ConfigureAwait(false);
+                // Read response as a string.
+                string result = await response.Content.ReadAsStringAsync();
+
+                // Deserialize the response using the classes created earlier.
+                TranslationResult[] deserializedOutput = JsonConvert.DeserializeObject<TranslationResult[]>(result);
+                // Iterate over the deserialized results.
+                foreach (TranslationResult o in deserializedOutput)
+                {
+                    // Print the detected input language and confidence score.
+                    Console.WriteLine("Detected input language: {0}\nConfidence score: {1}\n", o.DetectedLanguage.Language, o.DetectedLanguage.Score);
+                    // Iterate over the results and print each translation.
+                    foreach (Translation t in o.Translations)
+                    {
+                        Console.WriteLine("Translated to {0}: {1}", t.To, t.Text);
+                    }
+                }
+            }
+        }
+    }
+}
