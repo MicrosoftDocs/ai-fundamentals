792430f1b45387a6a60f8e96081954a91ca39093
Graeme Malcolm <gmalc@microsoft.com>
2020-05-18 12:17:17 -0700

    Custom Vision v 1

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -5,5 +5,5 @@ curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 mv /home/vsonline/workspace/ai-fundamentals/"WAIT." /home/vsonline/workspace/ai-fundamentals/"WAIT.."
 python3 get-pip.py
 mv /home/vsonline/workspace/ai-fundamentals/"WAIT.." /home/vsonline/workspace/ai-fundamentals/"WAIT..."
-pip install jupyter matplotlib pillow requests azure-cognitiveservices-vision-computervision azure-cognitiveservices-vision-customvision azure-cognitiveservices-vision-face azure-cognitiveservices-language-textanalytics azure.cognitiveservices.speech
+pip install jupyter matplotlib pillow requests azure-cognitiveservices-vision-computervision azure-cognitiveservices-vision-customvision==1.0.0 azure-cognitiveservices-vision-face azure-cognitiveservices-language-textanalytics azure.cognitiveservices.speech
 mv /home/vsonline/workspace/ai-fundamentals/"WAIT..." /home/vsonline/workspace/ai-fundamentals/"z REFRESH NOW!"
