0b73ba0c836afe11282b4fa525fb3e4d8ff3f9b2
tottokug <magcot.com@gmail.com>
2020-09-02 20:24:12 +0900

    .devcontainer

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,9 +1,2 @@
-echo -n "Setting up..." > /home/vsonline/workspace/ai-fundamentals/"WAIT"
 cd /usr/bin
-mv /home/vsonline/workspace/ai-fundamentals/"WAIT" /home/vsonline/workspace/ai-fundamentals/"WAIT."
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-mv /home/vsonline/workspace/ai-fundamentals/"WAIT." /home/vsonline/workspace/ai-fundamentals/"WAIT.."
-python3 get-pip.py
-mv /home/vsonline/workspace/ai-fundamentals/"WAIT.." /home/vsonline/workspace/ai-fundamentals/"WAIT..."
-pip install jupyter matplotlib pillow requests azure-cognitiveservices-vision-computervision azure-cognitiveservices-vision-customvision azure-cognitiveservices-vision-face azure-cognitiveservices-language-textanalytics azure.cognitiveservices.speech
-mv /home/vsonline/workspace/ai-fundamentals/"WAIT..." /home/vsonline/workspace/ai-fundamentals/"z REFRESH NOW!"
+echo -n "Ready to Go. Refresh the browser to start!"
